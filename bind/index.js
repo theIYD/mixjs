639d21500d97023b169a080ff882f1dedfa3cd9b
Idrees <dargahidrees@gmail.com>
2020-09-25 01:27:05 +0530

    Polyfill for bind

---
+++ bind/index.js
@@ -0,0 +1,8 @@
+// Polyfill for bind
+Function.prototype.customBind = function (...args) {
+  let context = this,
+    params = args.slice(1);
+  return function (...args2) {
+    context.apply(args[0], params.concat(args2));
+  };
+};
