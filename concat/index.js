75fb7823553c3e9cc3a208f4054f1d22989b614f
Idrees <dargahidrees@gmail.com>
2020-09-29 14:52:33 +0530

    lodash concat

--- concat/index.js
+++ concat/index.js
@@ -1,14 +1,18 @@
 Array.prototype.concat = function customConcat(...args) {
+  const resultant = [...this];
   const flatten = (arr) => {
     for (let i = 0; i < arr.length; i++) {
       if (Array.isArray(arr[i])) {
         flatten(arr[i]);
       } else {
-        this.push(arr[i]);
+        resultant.push(arr[i]);
       }
     }
     return;
   };
   flatten(args);
-  return this;
+  return resultant;
 };
+
+const arr = [1, 2];
+console.log(arrc.concat([4, 5]));
