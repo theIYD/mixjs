75687465b3b808037b3b242872153d069fefc98c
Idrees <dargahidrees@gmail.com>
2020-09-29 11:54:33 +0530

    promise.all implementation

---
+++ promise_all/index.js
@@ -0,0 +1,36 @@
+// Implementation of promise.all
+function all(promises) {
+  const _promises = promises.map((item) =>
+    item instanceof Promise ? item : Promise.resolve(item)
+  );
+
+  // resolve if empty
+  if (_promises.length === 0) {
+    return Promise.resolve([]);
+  }
+
+  return new Promise((resolve, reject) => {
+    const resultantArray = [];
+    let fulfilledCount = 0;
+    let isErrored = false;
+
+    _promises.forEach((promise, index) => {
+      promise.then(
+        (data) => {
+          if (isErrored) return;
+          resultantArray[index] = data;
+
+          fulfilledCount += 1;
+          if (fulfilledCount === _promises.length) {
+            resolve(resultantArray);
+          }
+        },
+        (error) => {
+          if (isErrored) return;
+          isErrored = true;
+          reject(error);
+        }
+      );
+    });
+  });
+}
