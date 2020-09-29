0fcadff6af873e3b7dea4b086f3209d510a41871
Idrees <dargahidrees@gmail.com>
2020-09-29 12:04:33 +0530

    implement promise.any()

---
+++ promise_any/index.js
@@ -0,0 +1,25 @@
+// Implement Promise.any()
+function any(promises) {
+  return new Promise((resolve, reject) => {
+    let isDone = false;
+    let errors = [];
+
+    promises.forEach((prom, index) => {
+      prom.then(
+        (data) => {
+          if (!isDone) {
+            resolve(data);
+            isDone = true;
+          }
+        },
+        (err) => {
+          errors[index] = err;
+
+          if (errors.length === promises.length) {
+            reject(new Error('none resolved'));
+          }
+        }
+      );
+    });
+  });
+}
