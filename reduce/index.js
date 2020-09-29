4028a84ed3a5c1c3f382795ac3790c97a3e09929
Idrees <dargahidrees@gmail.com>
2020-09-29 12:33:31 +0530

    polyfill for reduce

---
+++ reduce/index.js
@@ -0,0 +1,22 @@
+// Implementation of Array.prototype.reduce
+Array.prototype.reduce = function customReduce(reducer, initial) {
+  let acc = initial,
+    i = 0;
+
+  if (typeof initial === 'undefined') {
+    // Check if arr is empty
+    if (this.length === 0) {
+      throw new TypeError('empty array with initial value');
+    }
+
+    [acc] = this;
+    i = 1;
+  }
+
+  while (i < this.length) {
+    acc = reducer(acc, this[i], i, this);
+    i++;
+  }
+
+  return acc;
+};
