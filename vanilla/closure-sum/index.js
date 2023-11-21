6ea4103255519e31a9aed3c8cb849e2e5ec8b20d
Idrees Dargahwala <theiyd@Idreess-MacBook-Pro.local>
2023-11-21 22:17:07 +0530

    feat: closure sum

---
+++ vanilla/closure-sum/index.js
@@ -0,0 +1,14 @@
+function closureSum(accumulator) {
+    let a = accumulator;
+
+    function sum(current) {
+        a = a + current;
+        return a;
+    }
+
+    return sum
+}
+
+let sum = closureSum(10)
+console.log(sum(2)) // 12
+console.log(sum(3)) // 15 .. so on
