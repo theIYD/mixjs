e799ff980d6a8854d85ae2412534b77ae700e55c
Idrees Dargahwala <theiyd@Idreess-MacBook-Pro.local>
2023-12-13 20:50:12 +0530

    fix: retry promise with delay

--- vanilla/retry_promise/index.js
+++ vanilla/retry_promise/index.js
@@ -20,3 +20,33 @@ function retryPromise(fetcher, maxRetryCount) {
     storeFetcher();
   });
 }
+
+async function retryPromiseWithDelay(fetcher, delay, maxRetryCount) {
+  const wait = (ms) =>
+    new Promise((resolve) => {
+      setTimeout(() => resolve(), ms);
+    });
+
+  return new Promise((resolve, reject) => {
+    const storeFetcher = () => {
+      if (maxRetryCount > 0) {
+        fetcher().then(
+          (data) => {
+            resolve(data);
+          },
+          (err) => {
+            // retry
+            wait(delay).then(() => {
+              storeFetcher();
+              maxRetryCount--;
+            });
+          },
+        );
+      } else {
+        reject();
+      }
+    };
+
+    storeFetcher();
+  });
+}
